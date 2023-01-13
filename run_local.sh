46be422581db00d14245a0497abc8ea008a8bcde
johnbensnyder <johnbensnyder@gmail.com>
2023-01-13 23:11:59 +0000

    generic bucket

--- run_local.sh
+++ run_local.sh
@@ -1,6 +1,6 @@
 torchrun --nproc_per_node=4 src/train.py \
-    --train_data_src s3://jbsnyder-sagemaker-us-east/data/imagenet/train \
-    --val_data_src s3://jbsnyder-sagemaker-us-east/data/imagenet/val \
+    --train_data_src s3://[your-bucket]/data/imagenet/train \
+    --val_data_src s3://[your-bucket]/data/imagenet/val \
     --num_epochs 2 \
     --learning_rate 0.004 \
     --batch_size 128 \
